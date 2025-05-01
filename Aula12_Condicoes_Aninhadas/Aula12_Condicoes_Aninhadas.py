19931fa64d5500510fc4cf535f3bf8197cea68c3
SeuNome <leandroandrade020797@gmail.com>
2025-05-01 08:48:56 -0300

    Create Aula12_Condicoes_Aninhadas.py

---
+++ Aula12_Condicoes_Aninhadas/Aula12_Condicoes_Aninhadas.py
@@ -0,0 +1,9 @@
+n = str(input('Digite seu nome aqui: ')).lower()
+if n == "lelis":
+    print("Que nome lindo!")
+elif n == "michelle" or n == "rafaela":
+    print("Que belo nome feminino!")
+elif n in "paula bia manoel pâmela ":
+    print("Que nome legal!")
+else:
+    print("Que nome comum!")
