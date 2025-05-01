27385be3140da5051574e27d251fc095a93682b3
SeuNome <leandroandrade020797@gmail.com>
2025-05-01 17:28:14 -0300

    Update exercicio44_gerenciador_pagamentos.py

--- Aula12_Condicoes_Aninhadas/exercicio44_gerenciador_pagamentos.py
+++ Aula12_Condicoes_Aninhadas/exercicio44_gerenciador_pagamentos.py
@@ -5,9 +5,11 @@ n1 = int(input("Escolha uma opção: "))
 if n1 == 1:
     print("Sua compra de R${:.2f} vai custar R${:.2f} no final!".format(n,n-n*10/100))
 elif n1 == 2:
-    print("Sua compra de R${:.2f} vai custar R${:,2f} no final".format(n,n-n*5/100))
+    print("Sua compra de R${:.2f} vai custar R${:.2f} no final".format(n,n-n*5/100))
 elif n1 == 3:
-    print("Sua compra ficou no valor de R${} em 2x sem Juros".format(n))
+    print("Sua compra ficou no valor de R${:.2f} em 2x sem Juros".format(n))
 elif n1 == 4:
-    n2 = str(input("Quantas parcelas: "))
-    print()
+    n2 = int(input("Quantas parcelas: "))
+    if  2 < n2 <= 10:
+        n3 = (n + (n*20/100))/n2
+        print("Sua compra será parcelada em {}x de {}. Sua compra vai de {} para {} no final.".format(n2,n3,n,n+n*20/100))
