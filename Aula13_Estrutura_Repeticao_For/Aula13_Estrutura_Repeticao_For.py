89de2ad1231330d674585233b81adfbbfa29ebc7
SeuNome <leandroandrade020797@gmail.com>
2025-05-02 08:58:19 -0300

    commit

--- Aula13_Estrutura_Repeticao_For/Aula13_Estrutura_Repeticao_For.py
+++ Aula13_Estrutura_Repeticao_For/Aula13_Estrutura_Repeticao_For.py
@@ -1,4 +1,15 @@
-for c in range (6,0,-1):
+s = 0
+for c in range(0,6):
+    n = int(input("Digite um número: "))
+    s = s + n
+print("O somatório de todos os valores foi {}".format(s))
+print("Fim")
+    
+for c in range(0,5):
+    n = int(input("Digite um número: "))
+    print("Fim")
+
+for c in range (0,29,2):
     print(c)
 print("Fim")
 
@@ -16,4 +27,21 @@ for c in range (0,6):
 
 for c in range (0,6):
     print("Oi")
+print("Fim")
+
+i = int(input("Início: "))
+f = int(input("Fim: "))
+p = int(input("Passo: "))
+for c in range (i,f+1,p) :
+    print(c)
+print("Fim")
+
+n = int(input("Digite um número: "))
+for c in range(0,n+1):
+    print(c)
+print("Fim")
+
+n = int(input("Digite um número: "))
+for c in range(0,n):
+    print(c)
 print("Fim")
