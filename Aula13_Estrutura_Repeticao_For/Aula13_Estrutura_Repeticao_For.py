71f4f28db8e19c64ad571c6e6501eb3a643d4c28
SeuNome <leandroandrade020797@gmail.com>
2025-05-02 08:33:56 -0300

    Create Aula13_Estrutura_Repeticao_For.py

---
+++ Aula13_Estrutura_Repeticao_For/Aula13_Estrutura_Repeticao_For.py
@@ -0,0 +1,19 @@
+for c in range (6,0,-1):
+    print(c)
+print("Fim")
+
+for c in range (6,0,-1):
+    print(c)
+print("Fim")
+
+for c in range (1,16):
+    print(c)
+print("Fim")
+
+for c in range (0,6):
+    print("Oi")
+    print("Fim")
+
+for c in range (0,6):
+    print("Oi")
+print("Fim")
